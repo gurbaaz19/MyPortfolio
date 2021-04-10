27f7d5ac8dfac6d134719dd44c2732de699cdea0
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-04-10 18:22:11 +0530

    about-me

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function aboutMe(){
-let answer = prompt("What is your name??\nDont worry, it is not being recorded. You can cancel if you want.");
-console.log(`Name: ${answer}`);
+let name = prompt("What is your name??\nDont worry, it is not being recorded. You can cancel if you want.");
+console.log(`Name: ${name}`);
 if(answer!=null){
 let aboutMePara = document.getElementById("about-me-para");
-let aboutMeContent = `Hi ${answer}, I am Gurbaaz Singh Gill, I am a 1st year Computer Science Engineering student hailing from Ludhiana, Punjab. Currently I am exploring the world of programming and development, trying to learn new things from my mentors and seniors. I am currently focusing on Android Application Development using kotlin and working on small projects to improve my skills. To sum it up, I am beginner full of enthusiasm and a long way to go!!!`;
+let aboutMeContent = `Hi ${name}, I am Gurbaaz Singh Gill, a 1st year Computer Science Engineering student from Ludhiana, Punjab. Currently I am exploring the world of programming and development, trying to learn new things from my mentors and seniors. I am currently focusing on Android Application Development using kotlin and working on small projects to improve my skills. To sum it up, I am beginner full of enthusiasm and a long way to go!!!`;
 aboutMePara.innerText = aboutMeContent;
 }
 }
