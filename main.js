807f28c4175814ed0d1144c8a15314137cae0ecc
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-04-15 20:08:48 +0530

    added form

--- main.js
+++
@@ -1,9 +0,0 @@
-function aboutMe(){
-let name = prompt("What is your name??\nDont worry, it is not being recorded. You can cancel if you want.");
-console.log(`Name: ${name}`);
-if(name!=null){
-let aboutMePara = document.getElementById("about-me-para");
-let aboutMeContent = `Hi ${name}, I am Gurbaaz Singh Gill, a 1st year Computer Science Engineering student from Ludhiana, Punjab. Currently I am exploring the world of programming and development, trying to learn new things from my mentors and seniors. I am currently focusing on Android Application Development using kotlin and working on small projects to improve my skills. To sum it up, I am beginner full of enthusiasm and a long way to go!!!`;
-aboutMePara.innerText = aboutMeContent;
-}
-}
