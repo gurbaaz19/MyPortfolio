a9c1647c43c4e6a3be1461016240fcbdc7d093d0
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2021-04-17 17:55:03 +0530

    animated intro

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
+function change() {
+  if (
+    document.getElementById("animated-intro").getAttribute("src") ===
+    "Resources/intro.gif"
+  ) {
+    document
+      .getElementById("animated-intro")
+      .setAttribute("src", "Resources/intro1.gif");
+  } else {
+    document
+      .getElementById("animated-intro")
+      .setAttribute("src", "Resources/intro.gif");
+  }
+}
+
+
 var i = 0;
 var txt = "Welcome to my Website !!!!"; /* The text */
 var speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function welcome() {
-    if (i < txt.length) {
-      document.getElementById("welcome").innerHTML += txt.charAt(i);
-      i++;
-      setTimeout(welcome, speed);
-    }
+  if (i < txt.length) {
+    document.getElementById("welcome").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(welcome, speed);
+  }
 }
