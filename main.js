ea2b9bbe0dc1cd883eb40b303b002a4856d1f73a
Gurbaaz Singh Gill <gurbaaz19@gmail.com>
2022-07-05 19:50:02 +0530

    Changes

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 function change() {
-  if (
-    document.getElementById("animated-intro").getAttribute("src") ===
-    "Resources/intro.gif"
-  ) {
-    document
-      .getElementById("animated-intro")
-      .setAttribute("src", "Resources/intro1.gif");
-  } else {
-    document
-      .getElementById("animated-intro")
-      .setAttribute("src", "Resources/intro.gif");
-  }
+    if (
+        document.getElementById("animated-intro").getAttribute("src") ===
+        "Resources/icon.png"
+    ) {
+        document
+            .getElementById("animated-intro")
+            .setAttribute("src", "Resources/intro1.gif");
+    }
+        // else if (
+        //     document.getElementById("animated-intro").getAttribute("src") ===
+        //     "Resources/intro1.gif"
+        // ) {
+        //     document
+        //         .getElementById("animated-intro")
+        //         .setAttribute("src", "Resources/intro.gif");
+        //      }
+    else {
+        document
+            .getElementById("animated-intro")
+            .setAttribute("src", "Resources/icon.png");
+    }
 }
 
 
@@ -19,9 +28,9 @@ const txt = "Welcome to my Website !!!!"; /* The text */
 const speed = 50; /* The speed/duration of the effect in milliseconds */
 
 function welcome() {
-  if (i < txt.length) {
-    document.getElementById("welcome").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(welcome, speed);
-  }
+    if (i < txt.length) {
+        document.getElementById("welcome").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(welcome, speed);
+    }
 }
